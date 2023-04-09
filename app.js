b177da30360944bac5a17ff93951d3b7643198fa
Nayan More <morenayan81@gmail.com>
2023-04-09 18:01:35 +0530

    vercel file added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 config({ path: "./config/config.env" });
 const app = express();
@@ -10,6 +11,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 // Importing and use routes
 import user from "./routes/userRoute.js";
@@ -23,4 +31,11 @@ app.use("/api/v1", payment);
 app.use("/api/v1", other);
 
 export default app;
+
+app.get("/", (req, res) =>
+  res.send(
+    `<h1>Server is working. Click <a href="${process.env.FRONTEND_URL}">here</a> to visit frontend.</h1>`
+  )
+);
+
 app.use(ErrorMiddleware);
